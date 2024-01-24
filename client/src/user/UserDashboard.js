3c60011cb6c327b2b150967d8773c903fbe1c41c
Vaageesan V V <vaageesan04@gmail.com>
2024-01-24 17:48:21 +0530

    [Vaageesan] Change USD to INR

--- client/src/user/UserDashboard.js
+++ client/src/user/UserDashboard.js
@@ -77,7 +77,7 @@ const Dashboard = () => {
                     return (
                       <div key={i}>
                         <h6>Product name: {p.name}</h6>
-                        <h6>Product price: ${p.price}</h6>
+                        <h6>Product price: ₹{p.price}</h6>
                         <h6>Purchased date: {moment(p.createdAt).fromNow()}</h6>
                       </div>
                     );
